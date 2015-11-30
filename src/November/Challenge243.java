df680f47b7fecdf8df7cf8d0b9847c245d0720aa
Andrew  Paine <andrew.paine14@imperial.ac.uk>
2015-11-30 14:40:35 +0000

    Completed challenge for 30/11/15

---
+++ src/November/Challenge243.java
@@ -0,0 +1,27 @@
+package November;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Challenge243 {
+
+    /*Abundant and Deficient Numbers*/
+
+    public static void main(String[] args) throws IOException {
+        Scanner input = new Scanner(System.in);
+        while (input.hasNextInt()) {
+            int number = input.nextInt();
+            int sumOfDivisors = 0;
+            for (int i=1; i<number; i++)
+                if (number%i==0)
+                    sumOfDivisors += i;
+
+            if (sumOfDivisors<number)
+                System.out.println(number + " deficient");
+            else if (sumOfDivisors>number)
+                System.out.println(number + " abundant by " + (sumOfDivisors-number));
+            else
+                System.out.println(number + " ~~neither~~ deficient");
+        }
+    }
+}
